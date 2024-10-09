ca17fb2fed3073b7bc87c3c1e99d298c9857b491
babscsit104 <manzanarezb1@montclair.edu>
2024-10-09 16:25:10 +0000

    main code check

---
+++ favorite foods.py
@@ -0,0 +1,14 @@
+classmate_foods = ['hotdog', 'fries', 'nuggets', 'burger', 'pizza']
+continuer = True
+
+while continuer:
+    user_foods_1 = input('Enter your first favorite food : ')
+    user_foods_2 = input('Enter your second favorite food : ')
+    user_foods_3 = input('Enter your third favorite food : ')
+    for elements in list:
+        if user_foods_1 in list:
+            print('Our common favorite food(s): ')
+            break
+        else:
+            print("We have no favorite foods in common")
+            break
